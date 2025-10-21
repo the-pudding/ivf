54498ec72ffab0eb0cb9b090b6eb4e1abf6ec4b2
Michelle McGhee <michmac202@gmail.com>
2025-10-21 14:21:14 -0700

    update github pages

--- docs/_app/immutable/chunks/BHA3XBc5.js
+++
@@ -1,1 +0,0 @@
-import{l as t,f as e,g as c,u as f}from"./CDi5ttmh.js";function s(o){c===null&&t(),e(()=>{const n=f(o);if(typeof n=="function")return n})}export{s as o};
---
+++ docs/_app/immutable/chunks/D0uxJVOi.js
@@ -0,0 +1,1 @@
+import{l as t,u as f,f as e,g as c}from"./CwtTNJC4.js";function u(n){e===null&&t(),f(()=>{const o=c(n);if(typeof o=="function")return o})}function a(n){e===null&&t(),u(()=>()=>c(n))}export{a,u as o};
