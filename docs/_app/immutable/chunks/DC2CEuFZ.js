39f2e14b7a4f1e4ad542b0c86abe3a1641aed1ad
Michelle McGhee <michmac202@gmail.com>
2026-01-23 14:56:00 -0800

    update github pages

--- docs/_app/immutable/chunks/DC2CEuFZ.js
+++
@@ -1,1 +0,0 @@
-import{l as t,u as f,f as e,g as c}from"./BWtmqzP_.js";function u(n){e===null&&t(),f(()=>{const o=c(n);if(typeof o=="function")return o})}function a(n){e===null&&t(),u(()=>()=>c(n))}export{a,u as o};
